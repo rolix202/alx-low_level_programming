b79c197d73eb880e2c16e8fd5b7a41fd3137032a
rolix202 <williamsroland147@gmail.com>
2022-09-08 20:09:49 -0700

    update

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print the string in the put function
+ *
+ * Description: with proper grammar, but the outcome is a piece of art
+ * this program prints with proper grammar, but the outcome is a piece of art
+ * Return:0
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
+
+	printf("Size of a char: %id byte(s)\n", sizeof(c));
+	printf("Size of an int: %id byte(s)\n", sizeof(i));
+	printf("Size of a long int: %id byte(s)\n", sizeof(li));
+        printf("Size of a long long int: %id byte(s)\n" sizeof(lli));
+	return (0);
+}	

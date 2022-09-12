e8e41895d6a310642b95037c1fbed8bef99c7da0
rolix202 <williamsroland147@gmail.com>
2022-09-11 19:49:06 -0700

    update

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabets
+ * Description: prints using main function
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return = 0;
+}

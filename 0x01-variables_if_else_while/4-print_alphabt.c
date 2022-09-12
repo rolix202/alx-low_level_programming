d4d27ff0cf0a9e9ac7746410064fddc1acdec86d
rolix202 <williamsroland147@gmail.com>
2022-09-11 20:22:48 -0700

    update

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets except e
+ * Description: prints using main function
+ * Return:0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+	return (0);
+}

88a8f990648069e555a7067e1a74768f1e867fca
rolix202 <williamsroland147@gmail.com>
2022-09-11 20:52:07 -0700

    update

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints in lowercase
+ * Description: using main function
+ * this program prints in lowercase
+ * Return:0
+ */
+
+int main(void)
+{
+	char c;
+	
+	for (c = 'z'; c >= 'a'; c++)
+	{
+		putchar(c);
+	}
+	putchar("\n");
+	retrun (0);
+}

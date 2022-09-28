c9fdc01f3800c80c4bd5aab4c89d4448cca99e6a
rolix202 <williamsroland147@gmail.com>
2022-09-28 15:39:27 -0700

    update

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,5 +11,7 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int _putchar(char c);
+int is_prime(int n, int start);
+int _sqrt(int n, int x);
 
 #endif /*MAIN_H*/

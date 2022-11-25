af2ab84455b0b45cb641661b116b83fd7fc345a9
rolix202 <williamsroland147@gmail.com>
2022-11-25 00:47:37 +0000

    updated the 6-size.c file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,17 +7,20 @@
  * this program prints with proper grammar, but the outcome is a piece of art
  * Return:0
  */
+
 int main(void)
 {
-	char c;
-	int i;
-	long li;
-	long long lli;
-	float f;
+        int a = sizeof(char);
+        int b = sizeof(int);
+        int c = sizeof(long int);
+        int d = sizeof(float);
+        int e = sizeof(long long int);
 
-	printf("Size of a char: %ld byte(s)\n", sizeof(c));
-	printf("Size of an int: %ld byte(s)\n", sizeof(i));
-	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
-	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
-	return (0);
-}	
+        printf("Size of a char: %i byte(s)\n", a);
+        printf("Size of an int: %i byte(s)\n", b);
+        printf("Size of a long int: %i byte(s)\n", c);
+        printf("Size of a long long int: %i byte(s)\n", e);
+        printf("Size of a float: %i byte(s)\n", d);
+
+        return(0);
+}

c6f551fb54bdeaeb2fc7ab58bbe07aaf310acb5e
rolix202 <williamsroland147@gmail.com>
2022-11-25 23:10:45 +0000

    updated the 100-intel file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,15 @@
+
 #include <stdio.h>
 #include <unistd.h>
 
 /**
- * main - Entry Point
+ * main - Entry point
  *
- * Description: prints a string to the standard error
- *
- * Return: Always 1(Error)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-199\n";
-	write(2,  err, 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }

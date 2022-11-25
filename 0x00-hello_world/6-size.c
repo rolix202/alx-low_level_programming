3cc6edeed23949fd4f66c0c04ad2b0f919754ebb
rolix202 <williamsroland147@gmail.com>
2022-11-25 01:13:22 +0000

    update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -20,6 +20,6 @@ int main(void)
 	printf("Size of an int: %i byte(s)\n", b);
 	printf("Size of a long int: %i byte(s)\n", c);
 	printf("Size of a long long int: %i byte(s)\n", e);
-	printf("Size of a float: %i byte(s)\n", d);	
+	printf("Size of a float: %i byte(s)\n", d);
 	return (0);
 }

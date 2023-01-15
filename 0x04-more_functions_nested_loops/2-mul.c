5117a459cf694b7b31ea3691b7c754f9df3446d2
rolix202 <williamsroland147@gmail.com>
2023-01-15 22:50:59 +0100

    Write a function that multiplies two integers.

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - write a function that multiples two integers
+ *@a: first parameter
+ *@b: second parameter
+ * Return: a * b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}

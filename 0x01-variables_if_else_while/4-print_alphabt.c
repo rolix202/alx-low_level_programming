d4723b122b1ddaa3d0edafb0a9843218fa8c9463
rolix202 <williamsroland147@gmail.com>
2023-02-17 23:38:58 +0100

    script that prints the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char lower;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		if (lower == 'e' || lower == 'q')
+		{
+			continue;
+		}
+		putchar(lower);
+	}
+	putchar('\n');
+}

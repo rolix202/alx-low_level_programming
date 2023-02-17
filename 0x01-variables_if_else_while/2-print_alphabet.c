3e4bd2cd5d04276ed2c99c7d0546baaa46de0e1a
rolix202 <williamsroland147@gmail.com>
2023-02-17 15:43:24 +0100

    script that prints only the alphabet in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: returns alphabet in script that prints alphabet
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char i, a, z;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}

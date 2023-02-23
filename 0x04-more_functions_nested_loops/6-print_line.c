c6c55a17b06aa9767d21d0a1923cdd707bf8a83b
rolix202 <williamsroland147@gmail.com>
2023-02-23 19:02:05 +0100

    function that draws a straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - Entry point
+ * @n: variable int
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 1; i <= n; i++)
+	{
+		if (n <= 0)
+		{
+			_putchar('_');
+		}
+		_putchar('_');
+	}
+	_putchar('\n');
+}

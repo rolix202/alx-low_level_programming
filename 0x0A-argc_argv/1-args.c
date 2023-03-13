533d004cbe8d0573db1c2e77376ae800ed1ca52e
rolix202 <williamsroland147@gmail.com>
2023-03-13 08:31:31 +0100

    program that prints the number of value passed to it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: int
+ * @argv: char
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+
+	printf("%d\n", argc - 1);
+	return (0);
+}

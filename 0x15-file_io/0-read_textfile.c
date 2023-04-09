c8a81531d9d758f9eb7f8675aad1b1ca60ecfb72
rolix202 <williamsroland147@gmail.com>
2023-04-09 17:44:54 +0100

    function that reads a text file and prints it to the POSIX

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,28 +1,41 @@
 #include "main.h"
+#include <unistd.h>
+#include <fcntl.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-/**
- * read_textfile- Read text file print to STDOUT.
- * @filename: text file being read
- * @letters: number of letters to be read
- * Return: w- actual number of bytes read and printed
- *        0 when function fails or filename is NULL.
- */
+#define BUF_SIZE 1024
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buf;
 	ssize_t fd;
-	ssize_t w;
 	ssize_t t;
+	ssize_t w;
+	char *buffer;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
 
 	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
-	buf = malloc(sizeof(char) * letters);
-	t = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, t);
 
-	free(buf);
+	if (fd == -1)
+	{
+		return (0);
+	}
+
+	buffer = malloc(BUF_SIZE * sizeof(char));
+
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+	t = read(fd, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, t);
+
+	free(buffer);
 	close(fd);
 	return (w);
 }
